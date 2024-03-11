69cf9e77f27437dff07d543a158e23b86ab70ea5
KravAD <dkrav@myuax.com>
2024-03-11 19:27:33 +0100

    commit 31
    constructor de clase tour

--- Zoologico/src/main/java/interaccionVisitantes/tour.java
+++ Zoologico/src/main/java/interaccionVisitantes/tour.java
@@ -4,5 +4,14 @@ import habitats.habitats;
 import animales.animales;
 
 public class tour {
+    private habitats habitats;
+    private animales animales;
+
+    public tour(habitats habitats, animales animales) {
+        this.habitats = habitats;
+        this.animales = animales;
+    }
+
+    
 
 }
