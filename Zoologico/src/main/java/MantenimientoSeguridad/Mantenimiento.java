a04f5dd1bb422e5a67f714d380363c9d784bbba7
KravAD <dkrav@myuax.com>
2024-03-11 22:59:33 +0100

    commit 62
    atributos de constancia y prioridad y los constructores para ambos objetos.

--- Zoologico/src/main/java/MantenimientoSeguridad/Mantenimiento.java
+++ Zoologico/src/main/java/MantenimientoSeguridad/Mantenimiento.java
@@ -1,4 +1,10 @@
 package MantenimientoSeguridad;
+import java.util.Date;
 
 public class Mantenimiento extends Cuidar{
+    private String constancia;
+    public Mantenimiento(String nombre, String descripcion,Date fechaInicio, String estado, String constancia) {
+        super(nombre, descripcion,fechaInicio, estado);
+        this.constancia = constancia;
+    }
 }
--- Zoologico/src/main/java/MantenimientoSeguridad/RepararUrgente.java
+++ Zoologico/src/main/java/MantenimientoSeguridad/RepararUrgente.java
@@ -1,5 +1,11 @@
 package MantenimientoSeguridad;
+import java.util.Date;
 
 public class RepararUrgente extends Cuidar{
+    private String priorridad;
 
+    public RepararUrgente(String nombre, String descripcion, Date fechaInicio, String estado, String priorridad) {
+        super(nombre, descripcion, fechaInicio, estado);
+        this.priorridad = priorridad;
+    }
 }
