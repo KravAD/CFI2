45c0e269271c3be3b7907376c2f844bc33774e60
KravAD <dkrav@myuax.com>
2024-03-11 23:21:25 +0100

    commit 70
    getters y setters

--- Zoologico/src/main/java/MantenimientoSeguridad/ElementoSeguridad.java
+++ Zoologico/src/main/java/MantenimientoSeguridad/ElementoSeguridad.java
@@ -4,8 +4,24 @@ public class ElementoSeguridad {
     private String lugar;
     private boolean estado;
 
-    public ElementoSeguridad(String lugar, boolean estado) {
+    public ElementoSeguridad(String lugar) {
         this.lugar = lugar;
+        this.estado = true;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
         this.estado = estado;
     }
 }
