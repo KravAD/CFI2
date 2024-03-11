cfd0ce945b8cea4ccc5bf1b94508c664d667b9ab
KravAD <dkrav@myuax.com>
2024-03-11 23:17:31 +0100

    commit 67

--- Zoologico/src/main/java/MantenimientoSeguridad/registroTarea.java
+++ Zoologico/src/main/java/MantenimientoSeguridad/registroTarea.java
@@ -6,5 +6,16 @@ public class registroTarea {
     private List<Mantenimiento> tareasMantenimiento;
     private List<RepararUrgente> tareasReparacionUrgente;
 
-    
+
+    public registroTarea() {
+        this.tareasMantenimiento = new ArrayList<>();
+        this.tareasReparacionUrgente = new ArrayList<>();
+    }
+    public void agregarTareaMantenimiento(Mantenimiento mantenimiento) {
+        this.tareasMantenimiento.add(mantenimiento);
+    }
+
+    public void agregarTareaReparacionUrgente(RepararUrgente reparacionUrgente) {
+        this.tareasReparacionUrgente.add(reparacionUrgente);
+    }
 }
