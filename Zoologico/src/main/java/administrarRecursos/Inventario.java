e235672b551b9760eb400ae4e3dbc904a2f4f490
KravAD <dkrav@myuax.com>
2024-03-11 21:44:19 +0100

    commit 48
    Atributos y constructor del inventario.

--- Zoologico/src/main/java/administrarRecursos/Inventario.java
+++ Zoologico/src/main/java/administrarRecursos/Inventario.java
@@ -3,5 +3,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Inventario {
+    private List<Alimento> alimentos;
+    private List<Medicina> medicinas;
+    private List<Equipamiento> equipamientos;
 
+    public Inventario() {
+        this.alimentos = new ArrayList<>();
+        this.medicinas = new ArrayList<>();
+        this.equipamientos = new ArrayList<>();
+    }
+
+    
 }
