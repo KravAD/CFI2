74ce25df7126e69a476770a775b68e4e1cbbab9f
KravAD <dkrav@myuax.com>
2024-03-11 14:11:36 +0100

    commit 16
    constructor animales

--- Zoologico/src/main/java/animales/animales.java
+++ Zoologico/src/main/java/animales/animales.java
@@ -1,4 +1,13 @@
 package animales;
 
 public class animales {
+    private String especie;
+    private String nombre;
+    private int edad;
+
+    public animales(String especie, String nombre, int edad) {
+        this.especie = especie;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
 }
