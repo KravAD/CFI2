7e9daceba95ebc156cb6f0de55baae53f1da4e0f
KravAD <dkrav@myuax.com>
2024-03-11 23:35:42 +0100

    commit 75
    clase de sistema de seguridad

---
+++ Zoologico/src/main/java/MantenimientoSeguridad/SistemaSeguridad.java
@@ -0,0 +1,10 @@
+package MantenimientoSeguridad;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SistemaSeguridad {
+    private List<camara> camaras;
+    private List<sensor> sensores;
+
+    
+}
