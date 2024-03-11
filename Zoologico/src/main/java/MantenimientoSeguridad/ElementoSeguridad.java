2160b533c5ed559a9d8bbef544c0c885fdd13f06
KravAD <dkrav@myuax.com>
2024-03-11 23:20:26 +0100

    commit 69
    constructor de elemento de seguridad

--- Zoologico/src/main/java/MantenimientoSeguridad/ElementoSeguridad.java
+++ Zoologico/src/main/java/MantenimientoSeguridad/ElementoSeguridad.java
@@ -1,4 +1,11 @@
 package MantenimientoSeguridad;
 
 public class ElementoSeguridad {
+    private String lugar;
+    private boolean estado;
+
+    public ElementoSeguridad(String lugar, boolean estado) {
+        this.lugar = lugar;
+        this.estado = estado;
+    }
 }
