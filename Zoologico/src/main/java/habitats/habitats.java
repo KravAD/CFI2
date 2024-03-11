53c6773c71675132615533112aad895b8160fae0
KravAD <dkrav@myuax.com>
2024-03-11 12:57:35 +0100

    commit 4
    constructor de clase habitats

--- Zoologico/src/main/java/habitats/habitats.java
+++ Zoologico/src/main/java/habitats/habitats.java
@@ -4,4 +4,10 @@ public class habitats {
 private int temperatura;
 private int humedad;
 private boolean limpieza;
+
+    public habitats(int temperatura, int humedad, boolean limpieza) {
+        this.temperatura = temperatura;
+        this.humedad = humedad;
+        this.limpieza = limpieza;
+    }
 }
