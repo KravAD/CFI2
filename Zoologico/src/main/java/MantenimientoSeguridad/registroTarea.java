432d0b48f8feaae9ad5d805c514ffeb83d19414e
KravAD <dkrav@myuax.com>
2024-03-11 23:05:32 +0100

    commit 66
    atributos de la clase registorTarea

--- Zoologico/src/main/java/MantenimientoSeguridad/registroTarea.java
+++ Zoologico/src/main/java/MantenimientoSeguridad/registroTarea.java
@@ -3,5 +3,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class registroTarea {
+    private List<Mantenimiento> tareasMantenimiento;
+    private List<RepararUrgente> tareasReparacionUrgente;
 
+    
 }
