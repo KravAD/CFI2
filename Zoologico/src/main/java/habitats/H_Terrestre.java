97d5dd94c019c1d3179c8062d0ae45117c2b3c37
KravAD <dkrav@myuax.com>
2024-03-11 13:06:36 +0100

    commit 8
    constructor de la clase Terrestre

--- Zoologico/src/main/java/habitats/H_Terrestre.java
+++ Zoologico/src/main/java/habitats/H_Terrestre.java
@@ -1,4 +1,9 @@
 package habitats;
 
 public class H_Terrestre extends habitats{
+
+    public H_Terrestre(int temperatura, int humedad, boolean limpieza) {
+        super(temperatura, humedad, limpieza);
+    }
 }
+
