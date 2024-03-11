30314d95c77d2d68ed9f335bd6f2534dc118e67a
KravAD <dkrav@myuax.com>
2024-03-11 20:54:06 +0100

    commit 41
    getters y setters

--- Zoologico/src/main/java/administrarRecursos/Recurso.java
+++ Zoologico/src/main/java/administrarRecursos/Recurso.java
@@ -10,4 +10,28 @@ public class Recurso {
         this.cantidad = cantidad;
         this.descripcion = descripcion;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
