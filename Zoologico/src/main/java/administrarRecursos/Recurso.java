95411b3eae45afa8137d4966b2e96407bf73c1ad
KravAD <dkrav@myuax.com>
2024-03-11 20:51:39 +0100

    commit 40
    constructor

--- Zoologico/src/main/java/administrarRecursos/Recurso.java
+++ Zoologico/src/main/java/administrarRecursos/Recurso.java
@@ -5,7 +5,9 @@ public class Recurso {
     private int cantidad;
     private String descripcion;
 
-    
-
-
+    public Recurso(String nombre, int cantidad, String descripcion) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.descripcion = descripcion;
+    }
 }
