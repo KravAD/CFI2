e576bfd790131d6e0d91e6d5ab68cfdd0db25ccc
KravAD <dkrav@myuax.com>
2024-03-11 13:16:44 +0100

    commit 11
    Constructores de las clases Acuario y Aviario.

--- Zoologico/src/main/java/habitats/H_Acuatico.java
+++ Zoologico/src/main/java/habitats/H_Acuatico.java
@@ -1,4 +1,7 @@
 package habitats;
 
 public class H_Acuatico extends habitats{
+    public H_Acuatico(int temperatura, int humedad, boolean limpieza) {
+        super(temperatura, humedad, limpieza);
+    }
 }
--- Zoologico/src/main/java/habitats/H_Aviario.java
+++ Zoologico/src/main/java/habitats/H_Aviario.java
@@ -1,4 +1,7 @@
 package habitats;
 
 public class H_Aviario extends habitats{
+    public H_Aviario(int temperatura, int humedad, boolean limpieza) {
+        super(temperatura, humedad, limpieza);
+    }
 }
