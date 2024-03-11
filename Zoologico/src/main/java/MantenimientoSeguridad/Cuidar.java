cbbebc25177469b3d0a5d285554335480ebb7c5a
KravAD <dkrav@myuax.com>
2024-03-11 22:50:41 +0100

    commit 59
    getters y setters

--- Zoologico/src/main/java/MantenimientoSeguridad/Cuidar.java
+++ Zoologico/src/main/java/MantenimientoSeguridad/Cuidar.java
@@ -14,5 +14,36 @@ public class Cuidar {
         this.fechaInicio = fechaInicio;
         this.estado = estado;
     }
-    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
