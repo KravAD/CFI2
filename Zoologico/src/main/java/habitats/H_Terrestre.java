e231d57f95c2b73c355fad9f13a9b4c67ea10d7b
KravAD <dkrav@myuax.com>
2024-03-11 13:07:05 +0100

    commit 9
    atributo de nombre del habitat

--- Zoologico/src/main/java/habitats/H_Terrestre.java
+++ Zoologico/src/main/java/habitats/H_Terrestre.java
@@ -1,6 +1,7 @@
 package habitats;
 
 public class H_Terrestre extends habitats{
+    private String nombre;
 
     public H_Terrestre(int temperatura, int humedad, boolean limpieza) {
         super(temperatura, humedad, limpieza);
