5d8a8fc7dbb41d49bdfe87a79ee0bd99f4fde3f3
KravAD <dkrav@myuax.com>
2024-03-11 22:49:25 +0100

    commit 57

--- Zoologico/src/main/java/MantenimientoSeguridad/Cuidar.java
+++ Zoologico/src/main/java/MantenimientoSeguridad/Cuidar.java
@@ -1,8 +1,12 @@
 package MantenimientoSeguridad;
+import java.util.Date;
+
 
 public class Cuidar {
     private String nombre;
     private String descripcion;
     private Date fechaInicio;
     private String estado;
+
+    
 }
