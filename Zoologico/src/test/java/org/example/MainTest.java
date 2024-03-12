637041caaa13ef01cf281910f8d94221caea7a85
KravAD <dkrav@myuax.com>
2024-03-12 15:31:00 +0100

    commit 80
    importes de clases

--- Zoologico/src/test/java/org/example/MainTest.java
+++ Zoologico/src/test/java/org/example/MainTest.java
@@ -1,5 +1,13 @@
 package org.example;
 
+import habitats.*;
+import animales.*;
+import administrarRecursos.*;
+import interaccionVisitantes.*;
+import MantenimientoSeguridad.*;
+import java.util.Date;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
